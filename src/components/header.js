ae44749d624b8238acfb7ba67a136162133184cc
ViktoriaV <august.flamenco@gmail.com>
2023-10-19 00:09:30 +0300

    header

---
+++ src/components/header.js
@@ -0,0 +1,40 @@
+import {HEADER_SELECTOR} from "@/config/constants";
+
+
+
+export class Header {
+
+  headerContainer = document.querySelector(HEADER_SELECTOR);
+
+  constructor() {
+    this._render();
+    this._init();
+  }
+
+  _markUp() {
+    return `
+      <div class="container header__wrap">
+        <div class="header__icon-catalog"></div>
+        <div class="header__logo">ТОВАРЫ И ТОЧКА</div>
+        <div class="header__search">
+          <input type="text" name="search" placeholder="Я ищу..." class="header__input">
+          <button class="header__btn"></button>
+        </div>
+        <div class="header__profile">
+          <span class="header__label">Профиль</span>
+        </div>
+        <div class="header__cart">
+          <span class="header__label">Корзина</span>
+          <div class="header__quantity">3</div>
+        </div>
+        
+      </div>
+    
+    `;
+  }
+
+  _render() {
+    this.headerContainer.insertAdjacentHTML("afterbegin", this._markUp());
+  }
+  _init() {}
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import '@/styles/index.scss';
+import {Header} from "@/components/header";
+
+const header = new Header();
+
+
+console.log(555);
